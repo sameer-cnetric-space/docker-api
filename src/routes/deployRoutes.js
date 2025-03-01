03ed302e1b5fda5663a3c7d6aac75ea52cc469e4
sameer-cnetric-space <sameer@cnetric.com>
2025-03-01 16:50:14 +0530

    Update deployRoutes.js

--- src/routes/deployRoutes.js
+++ src/routes/deployRoutes.js
@@ -3,6 +3,10 @@ const { deployDockerCompose } = require("../controllers/deployController");
 
 const router = express.Router();
 
+router.get("/hello", (req, res) => {
+  return res.status(200).json({ message: "Hello World" });
+});
+
 router.post("/", deployDockerCompose);
 
 module.exports = router;
